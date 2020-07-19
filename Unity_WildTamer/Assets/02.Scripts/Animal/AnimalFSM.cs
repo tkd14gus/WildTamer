5ad3d9c97f68c782248885fc67352c042e9ba0ca
tkd14gus <qkrtkdgus78@gmail.com>
2020-07-20 01:03:31 +0900

    AnimalFSM 간략하게 추가
    2020-07-20  01:03
    
    - AnimalFSM에 Animator 컴포넌트
    - Update문 안에 Switch로 만들고, 각 함수 틀 생성

--- Unity_WildTamer/Assets/02.Scripts/Animal/AnimalFSM.cs
+++ Unity_WildTamer/Assets/02.Scripts/Animal/AnimalFSM.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,8 @@ public class AnimalFSM : MonoBehaviour
 
     AnimalState anis = AnimalState.Default;
 
+    private Animator anim;
+
     public int hp = 100;
     public float speed = 1.0f;
     public float attackTime = 1.5f;
@@ -20,12 +23,47 @@ public class AnimalFSM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //애니메이터 컴포넌트
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (anis)
+        {
+            case AnimalState.Default:
+                Default();
+                break;
+            case AnimalState.Run:
+                Run();
+                break;
+            case AnimalState.Attack:
+                Attack();
+                break;
+            case AnimalState.Down:
+                Down();
+                break;
+        }
+    }
+
+    private void Default()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Run()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Down()
+    {
+        throw new NotImplementedException();
     }
 }
