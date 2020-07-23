4d8bd1ca7de8b4ea419e7abf871e9b70f1759b98
tkd14gus <qkrtkdgus78@gmail.com>
2020-07-23 15:45:23 +0900

    미니맵과 전장의 안개 구현
    2020-07-23  15 : 45
    
    - 미니맵 구현
    - 전장의 안개 구현
    - 아직 씬간 이동을 하면 찾아갔던 안개 모두 되살아남.(데이터 저장 요구됨)

---
+++ Unity_WildTamer/Assets/02.Scripts/Utility/MiniMapCamRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamRotation : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.rotation.y == 180)
+        {
+            Vector3 dis = new Vector3(player.transform.position.x, player.transform.position.y, 10);
+            transform.position = dis;
+            Vector3 ro = new Vector3(0, -180, 0);
+            transform.rotation = Quaternion.Euler(ro);
+        }
+        else
+        {
+            Vector3 dis = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+            transform.position = dis;
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+    }
+}
---
+++ Unity_WildTamer/Assets/02.Scripts/Utility/MiniMapMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapMove : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = player.transform.position;
+        transform.position = pos;
+    }
+}
